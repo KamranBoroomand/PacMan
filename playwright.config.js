998c85a23810bb5b1e5a4f3e4a2a4aaa8c4e2fcb
Kamran Boroomand <50832086+KamranBoroomand@users.noreply.github.com>
2026-02-24 10:49:58 +0300

    Run playwright test desktop chromium

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   testDir: "./tests/e2e",
   timeout: 30000,
   retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 1 : undefined,
   expect: {
     toHaveScreenshot: {
       animations: "disabled",
